99c5aad78f6897b0644475a14484542f702ae02f
Daniel George <programmerpremium@gmail.com>
2022-12-05 20:47:42 +0000

    Added mantine and login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AppShell, Button, Container, MantineProvider, PasswordInput, Stack, Stepper, TextInput, useMantineTheme } from "@mantine/core";
+import { useState } from "react";
 
 function App() {
+	const theme = useMantineTheme();
+	const [activeStep, setActiveStep] = useState(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: "light",
+        primaryColor: "dark",
+      }}
+    >
+      <AppShell padding="lg">
+        <Container size={800}>
+          <Stepper
+            active={activeStep}
+            onStepClick={setActiveStep}
+            breakpoint="sm"
+          >
+						<Stepper.Step label="Login" description="Login with Seneca">
+              <Container size={400}>
+								<Stack my="lg" spacing={0}>
+									<TextInput label="Username" required />
+									<PasswordInput label="Password" required />
+									<Button my="lg" onClick={() => setActiveStep(activeStep + 1)}>Login</Button>
+                </Stack>
+              </Container>
+            </Stepper.Step>
+            <Stepper.Step label="Courses" description="Select a target course">
+              Step 2 content
+            </Stepper.Step>
+            <Stepper.Step label="Generation" description="Tinker with settings">
+              Step 3 content
+            </Stepper.Step>
+          </Stepper>
+        </Container>
+      </AppShell>
+    </MantineProvider>
   );
 }
 
